efd1eecff3281fdb83b10d53cdae4d4a0bf853e0
Renato <renatopjesus1818@gmail.com>
2024-07-25 13:37:47 +0100

    updates plot

--- src/plot_results.py
+++ src/plot_results.py
@@ -9,7 +9,7 @@ results_files = [
 ]  # List of result files
 plot_title = "Performance Metrics Comparison"  # Title of the plot
 output_image = "performance_metrics_comparison.png"  # Output image file name
-add_trendline = True  # Set to True to add a trendline
+add_trendline = False  # Set to True to add a trendline
 
 # Read results from the file
 def read_results(file_path):
@@ -39,44 +39,61 @@ def read_results(file_path):
 def plot_results(results_data, plot_title, output_image, add_trendline):
     fig, ax1 = plt.subplots(figsize=(12, 6))
 
-    colors = ['gray', 'lightgray']
-    labels = []
+    # Plot the first dataset on the primary Y-axis
+    test_names, min_times, max_times, avg_times, _ = zip(*results_data[0])
+    x = np.arange(len(test_names))  # the label locations for the first dataset
 
-    # Create a secondary y-axis
+    # Plot the average bars for the first dataset
+    bar_width = 0.35
+    bars_avg1 = ax1.bar(x - bar_width / 2, avg_times, bar_width, label='Average - Dataset 1', color='gray')
+
+    # Add the variation between min and max
+    for i in range(len(x)):
+        ax1.vlines(x[i] - bar_width / 2, min_times[i], max_times[i], color='black', linestyle='dashed')
+
+    # Optional trendline for the first dataset
+    if add_trendline:
+        z = np.polyfit(x, avg_times, 1)
+        p = np.poly1d(z)
+        ax1.plot(x, p(x), "--", color='black', label='Trendline - Dataset 1')
+
+    # Create a secondary Y-axis
     ax2 = ax1.twinx()
 
-    bar_width = 0.35
-    for index, data in enumerate(results_data):
-        test_names, min_times, max_times, avg_times, _ = zip(*data)
-        x = np.arange(len(test_names))  # the label locations
+    # Plot the second dataset on the secondary Y-axis
+    test_names2, min_times2, max_times2, avg_times2, _ = zip(*results_data[1])
+    x2 = np.arange(len(test_names2))  # the label locations for the second dataset
 
-        # Plot the average bars
-        bars_avg = ax1.bar(x + index * bar_width, avg_times, bar_width, label=f'Average - Dataset {index + 1}', color=colors[index])
+    # Plot the average bars for the second dataset
+    bars_avg2 = ax2.bar(x2 + bar_width / 2, avg_times2, bar_width, label='Average - Dataset 2', color='lightgray')
 
-        # Add the variation between min and max
-        for i in range(len(x)):
-            ax1.vlines(x[i] + index * bar_width, min_times[i], max_times[i], color='black', linestyle='dashed')
+    # Add the variation between min and max for the second dataset
+    for i in range(len(x2)):
+        ax2.vlines(x2[i] + bar_width / 2, min_times2[i], max_times2[i], color='black', linestyle='dashed')
 
-        # Optional trendline
-        if add_trendline:
-            z = np.polyfit(x, avg_times, 1)
-            p = np.poly1d(z)
-            ax1.plot(x + index * bar_width, p(x), "--", color='black', label=f'Trendline - Dataset {index + 1}')
+    # Optional trendline for the second dataset
+    if add_trendline:
+        z = np.polyfit(x2, avg_times2, 1)
+        p = np.poly1d(z)
+        ax2.plot(x2, p(x2), "--", color='black', label='Trendline - Dataset 2')
 
-        labels.append(f'Dataset {index + 1}')
-
-    # Add some text for labels, title, and custom x-axis tick labels, etc.
+    # Add labels, title, and custom x-axis tick labels
     ax1.set_xlabel('Test Case')
-    ax1.set_ylabel('Time (s)')
+    ax1.set_ylabel('Time (s) - Dataset 1')
+    ax2.set_ylabel('Time (s) - Dataset 2')
     ax1.set_title(plot_title)
-    ax1.set_xticks(x + (len(results_data) - 1) * bar_width / 2)
-    ax1.set_xticklabels(test_names)
-    ax1.legend(loc='upper left')
 
-    # Add the secondary y-axis label (example purpose)
-    ax2.set_ylabel('Secondary Metric')  # You can change this label according to your needs
+    # Adjust x-ticks to fit both datasets
+    combined_x = np.concatenate([x - bar_width / 2, x2 + bar_width / 2])
+    combined_labels = list(test_names) + list(test_names2)
+    ax1.set_xticks(combined_x)
+    ax1.set_xticklabels(combined_labels, rotation=45, ha='right')
 
-    # Adjust layout and save image
+    # Combine legends from both axes
+    lines, labels = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax1.legend(lines + lines2, labels + labels2, loc='upper left')
+
     fig.tight_layout()
     plt.grid(True)
     plt.savefig(output_image)
